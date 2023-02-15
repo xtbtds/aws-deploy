ad16cb280a34c4f5489e90a3fde6057f99b3c6fa
laniakea <liza.xtbtds@gmail.com>
2023-02-15 16:11:46 +0100

    ready aws deploy

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -52,7 +52,7 @@ class Fib extends Component {
         return (
             <div>
                 <form onSubmit={this.handleSubmit}> 
-                    <label>Enter your index:</label>
+                    <label>Enter your index here:</label>
                     <input 
                         value={this.state.index}
                         onChange={event => this.setState({ index: event.target.value })}
